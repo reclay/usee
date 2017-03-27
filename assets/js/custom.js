2ab6f284745bce01408f64caefef1c8f060f331d
clay <2868902382@qq.com>
2017-03-27 22:13:11 +0800

    v1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,8 @@
-$(".navbar-nav li").click(function(){
-    if(!$(".navbar-toggle").is(":hidden")){
-        $(".navbar-collapse").collapse('hide');
-    }
+$(function() {
+    // $("img.lazy").lazyload();
+    $(".navbar-nav li").click(function(){
+        if(!$(".navbar-toggle").is(":hidden")){
+            $(".navbar-collapse").collapse('hide');
+        }
+    });
 });
